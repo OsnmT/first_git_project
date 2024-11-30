244e5d4aaa5a0b09f5e21e829d9ddce5a5ac0d12
OsnmT <gulsahcostur@gmail.com>
2024-11-30 19:54:51 +0300

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -6,6 +6,9 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo");
+        System.out.println("Hello Cydeo");
+        // some change are added;
+
 
     }
 }
